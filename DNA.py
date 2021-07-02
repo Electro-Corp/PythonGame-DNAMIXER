2207bb64fa027f9a232b3363c30a499a67ed78de
Electro-Corp <chinmaytanmay@outlook.com>
2021-07-02 11:12:31 -0500

    Add files via upload

---
+++ DNA.py
@@ -0,0 +1,40 @@
+print("DNA MIXER")
+print("VER 1.0 ALPHA")
+print("(C)2019-2020 Electro Corp")
+print("(^.^)")
+print("Here are options:")
+print("S = Spider, F = Fly, C = Computer Chip, O = Owl")
+print(" L = Lizerd, CA = Cameleon, CT = cat, W = Wolverine")
+print("E = Elephant, R = Rhino")
+print("These are case sensitive")
+GAME = True
+while GAME == True:
+    v = input("FIRST")
+    a = input("SECOND")
+    if v == "F":
+        if a == "S":
+            print("Good.. You nade spiderman capabilitys...")
+        else:
+            print("FAIL! DO BETTER...")
+    
+    if v == "O":
+        if a == "C":
+            print("Good.. You nade SMART brain capabilitys...")
+        else:
+            print("FAIL! DO WORSE.. Ha ha")
+    if v == "W":
+        if a == "CT":
+            print("Good.. You nade Claw capabilitys...")
+        else:
+            print("FAIL! Play SOMETHING ELSE")
+    if v == "R":
+        if a == "E":
+            print("Good.. You nade tusk capabilitys...")
+        else:
+            print("FAIL! Come on..")
+    if v == "CA":
+        if a == "L":
+            print("Good.. You nade INVISIBLITY capabilitys...")
+        else:
+            print("FAIL! Why you fail me?")
+    
